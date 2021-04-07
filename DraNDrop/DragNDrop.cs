5ee446c116404a183507505381ce9bfdc9e6ed7a
Braxoia <ibrahime.ahbib@etu.u-paris.fr>
2021-04-07 22:04:58 +0200

    Mise en place d'un IU

---
+++ DraNDrop/DragNDrop.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+
+namespace DraNDrop
+{
+    public class DragNDrop
+    {
+        public static void DNDrop(string input_path)
+        {
+            string[] path = input_path.Split(@"\"); //@ = échape les caractères de chemin de fichier 
+            string nameFile = path[^1]; //une case à partir de la fin 
+           
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                if (drive.Name == @"C:\")
+                {
+                    continue;
+                }
+                
+                string destFileName = $"{drive.Name}{nameFile}";
+                
+                FileAttributes attr = File.GetAttributes(input_path);
+                if ((attr & FileAttributes.Directory) != 0)
+                {
+                    DirectoryCopy(input_path, destFileName, true);
+                }
+                else
+                {
+                    try
+                    {
+                        File.Copy(input_path, destFileName, true);
+                    }
+                    catch (FileNotFoundException e)
+                    {
+                        Console.Error.WriteLine("Ce fichier n'existe pas ! (Remerciez Ibrahime ;-)");
+                    }
+                }
+            }
+        }
+        
+        public static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+            Console.WriteLine(sourceDirName);
+            
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+        
+            // If the destination directory doesn't exist, create it.       
+            Directory.CreateDirectory(destDirName);        
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string tempPath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(tempPath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string tempPath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, tempPath, copySubDirs);
+                }
+            }
+        }
+    }
+}
---
+++ GUI/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+                    Console.WriteLine(filePath);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var directoryPath = string.Empty;
+            
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    directoryPath = folderBrowserDialog.SelectedPath;
+                    DraNDrop.DragNDrop.DNDrop(directoryPath);
+                    Console.WriteLine(directoryPath);
+                }
+            }
+        }
+    }
+} 
